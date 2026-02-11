f7bfb535b7a97215c60fd202e7092672be90a1f9
Nourelhoudabenali <nourb8629@gmail.com>
2026-02-11 20:42:09 +0100

    pet git commit --amend --reset-author

---
+++ src/Enum/PetGender.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enum;
+
+enum PetGender: string
+{
+    case MALE = 'MALE';
+    case FEMALE = 'FEMALE';
+    case STERILIZED = 'STERILIZED';
+}
---
+++ src/Enum/TypeGuard.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enum;
+
+enum TypeGuard: string
+{
+    case CHEZ_PROPRIETAIRE = 'CHEZ_PROPRIETAIRE';
+}
